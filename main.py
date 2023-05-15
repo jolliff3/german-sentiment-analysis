a2bfbf8413b5fbe7c1f90bb78746d673fdebf464
Jeremy <jeremy.olliffe@gmail.com>
2023-05-15 13:37:08 +1000

    Allow for use of queries via the api

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from germansentiment import SentimentModel
 import requests
 import matplotlib.pyplot as plt
 import csv
+import json
 
 
 class Speech:
@@ -17,13 +18,27 @@ class Speech:
     sentiment_model = None
     api_url = None
 
-    def __init__(self, url, sentiment_model):
-        self.api_url = url
-        self.speech_raw_json = requests.get(self.api_url).json()
+    def __init__(self, url=None, speech_raw_json=None):
+        if url is None and speech_raw_json is None:
+            raise ValueError("Must provide either url or speech_raw_json")
+        if url is not None and speech_raw_json is not None:
+            raise ValueError(
+                "Must provide either url or speech_raw_json, not both")
+        if url is not None:
+            self.__init_from_url(url)
+        else:
+            self.__init_from_speech_raw_json(speech_raw_json)
+
         self.sentences = self.__parse_sentences()
         self.speech_metadata = self.__parse_speech_metadata()
-        self.sentiment_model = sentiment_model
-        self.__analyse_sentiment()
+
+    def __init_from_url(self, url):
+        self.api_url = url
+        self.speech_raw_json = requests.get(self.api_url).json()
+
+    def __init_from_speech_raw_json(self, speech_raw_json):
+        self.speech_raw_json = speech_raw_json
+        self.api_url = "unknown"
 
     def __parse_speech_metadata(self):
         speech_metadata = {}
@@ -37,8 +52,24 @@ class Speech:
         # To find the main speaker, we itterate through the sentences until we find the first one with the speaker status "main-speaker"
         for sentence in self.sentences:
             if sentence["speaker_status"] == "main-speaker":
-                speech_metadata["main_speaker"] = sentence["speaker"]
+                if sentence["speaker"] is None:
+                    speech_metadata["main_speaker"] = "unknown"
+                else:
+                    speech_metadata["main_speaker"] = sentence["speaker"]
                 break
+
+        if "main_speaker" not in speech_metadata:
+            speech_metadata["main_speaker"] = "unknown"
+
+        # Find the person data using the speech_metadata["main_speaker"]
+        if speech_metadata["main_speaker"] == "unknown":
+            speech_metadata["main_speaker_party"] = "unknown"
+        else:
+            for person in self.speech_raw_json["data"]["relationships"]["people"]["data"]:
+                if person["attributes"]["label"] == speech_metadata["main_speaker"]:
+                    speech_metadata["main_speaker_party"] = person["attributes"]["party"]["label"]
+                    break
+
         return speech_metadata
 
     def __parse_sentences(self):
@@ -51,6 +82,11 @@ class Speech:
             speech_excerpt_sentences = speech_excerpt_with_metadata["sentences"]
             for speech_excerpt_sentence in speech_excerpt_sentences:
                 speech_excerpt_sentence["type"] = speech_excerpt_with_metadata["type"]
+
+                if "timeStart" not in speech_excerpt_sentence:
+                    speech_excerpt_sentence["timeStart"] = 0
+                if "timeEnd" not in speech_excerpt_sentence:
+                    speech_excerpt_sentence["timeEnd"] = 0
                 speech_excerpt_sentence["duration"] = self.__calculate_sentence_length_seconds(
                     speech_excerpt_sentence)
 
@@ -70,9 +106,9 @@ class Speech:
 
         return speech_sentences_with_timestamps
 
-    def __analyse_sentiment(self):
+    def analyse_sentiment(self, sentiment_model):
         sentences_text = [sentence["text"] for sentence in self.sentences]
-        result = self.sentiment_model.predict_sentiment(
+        result = sentiment_model.predict_sentiment(
             sentences_text, True)
         sentiment_scores = result[0]
         sentiment_weights = result[1]
@@ -88,8 +124,12 @@ class Speech:
 
     def __calculate_sentence_length_seconds(self, sentence):
         # The timestamps are in seconds with 3 decimal places and are strings
-        timeEnd = float(sentence["timeEnd"])
-        timeStart = float(sentence["timeStart"])
+        try:
+            timeEnd = float(sentence["timeEnd"])
+            timeStart = float(sentence["timeStart"])
+        except:
+            print("invalid sentence: " + json.dumps(sentence))
+            raise ValueError("Invalid sentence")
 
         # Should return to 3dp
         return timeEnd - timeStart
@@ -117,8 +157,9 @@ class Speech:
         return self.speech_metadata
 
 
-def analyse_speech(speech_url, sentiment_model):
-    speech = Speech(speech_url, sentiment_model)
+def analyse_speech(speech, sentiment_model):
+    speech.analyse_sentiment(sentiment_model)
+
     sentences = speech.get_sentences_by_speaker_status("main-speaker")
 
     total_duration = 0
@@ -144,57 +185,101 @@ def analyse_speech(speech_url, sentiment_model):
         "duration_postive": duration_postive,
         "duration_neutral": duration_neutral,
         "duration_negative": duration_negative,
-        "percentage_positive": duration_postive / total_duration,
-        "percentage_neutral": duration_neutral / total_duration,
-        "percentage_negative": duration_negative / total_duration,
         "agenda_item_title": metadata["agenda_item_title"],
         "main_speaker": metadata["main_speaker"],
+        "main_speaker_party": metadata["main_speaker_party"],
     }
 
+    if total_duration != 0:
+        analysis_results["percentage_positive"] = duration_postive / \
+            total_duration
+        analysis_results["percentage_neutral"] = duration_neutral / \
+            total_duration
+        analysis_results["percentage_negative"] = duration_negative / \
+            total_duration
+    else:
+        analysis_results["percentage_positive"] = 0
+        analysis_results["percentage_neutral"] = 0
+        analysis_results["percentage_negative"] = 0
     return analysis_results
 
 
-def create_csv_from_analysis_results(array_of_analysis_results):
-    with open('resultsTest.csv', 'w', newline='') as file:
+def create_csv_from_analysis_results(array_of_analysis_results, filename="results.csv"):
+    path = "results/" + filename
+
+    with open(path, 'w', newline='') as file:
         writer = csv.writer(file)
         field = ["total_duration", "duration_postive", "duration_neutral", "duration_negative",
-                 "percentage_positive", "percentage_neutral", "percentage_negative", "agenda_item_title", "main_speaker"]
+                 "percentage_positive", "percentage_neutral", "percentage_negative", "agenda_item_title", "main_speaker", "main_speaker_party"]
         writer.writerow(field)
         for analysis_result in array_of_analysis_results:
             writer.writerow([analysis_result["total_duration"], analysis_result["duration_postive"], analysis_result["duration_neutral"],
                              analysis_result["duration_negative"], analysis_result[
                                  "percentage_positive"], analysis_result["percentage_neutral"],
-                             analysis_result["percentage_negative"], analysis_result["agenda_item_title"], analysis_result["main_speaker"]])
+                             analysis_result["percentage_negative"], analysis_result["agenda_item_title"], analysis_result["main_speaker"], analysis_result["main_speaker_party"]])
+
+
+def get_speeches_by_query(query, limit=None):
+    base_url = "https://de.openparliament.tv/api/v1/search/media"
+    query_url = base_url + "?q=" + query
+    speeches_raw_json = requests.get(query_url).json()
+    speeches = []
+
+    current_speech = 0
+    for speech_data in speeches_raw_json["data"]:
+        if limit is not None and current_speech >= limit:
+            break
+        current_speech += 1
+        speech_data_formatted = {}
+        speech_data_formatted["data"] = speech_data
+
+        speeches.append(Speech(speech_raw_json=speech_data_formatted))
+
+    return speeches
 
 
 def main():
+    # speeches = get_speeches_by_query("Ausländer", 20)
+    speeches = get_speeches_by_query("Budget", 20)
     sentiment_model = SentimentModel()
-    speech_urls = [
-        # Martin SichertAfD - Impfpflicht gegen SARS-CoV-2 https://de.openparliament.tv/media/DE-0200028012?q=AFD&factionID%5B%5D=Q42575708
-        "https://de.openparliament.tv/api/v1/media/DE-0200028012",
-        # Nicole HöchstAfD - Akademische und berufliche Bildung https://de.openparliament.tv/media/DE-0200103063?q=AFD&factionID%5B%5D=Q42575708
-        "https://de.openparliament.tv/api/v1/media/DE-0200103063",
-        # Gottfried CurioAfD - Durchsetzung des Asyl- und Aufenthaltsrechts https://de.openparliament.tv/media/DE-0200103033?q=AFD&factionID%5B%5D=Q42575708
-        "https://de.openparliament.tv/api/v1/media/DE-0200103033",
-        # NOT AFD https://de.openparliament.tv/api/v1/media/DE-0190204119
-        "https://de.openparliament.tv/api/v1/media/DE-0190204119",
-        # Martin HessAfD - Kriminalität in Bahnhöfen und Zügen https://de.openparliament.tv/media/DE-0200087057?q=Jahr+2022+um+38%2C6%25+von+rund&factionID%5B%5D=Q42575708&dateFrom=2022-10-15
-        "https://de.openparliament.tv/api/v1/media/DE-0200087057",
-        # Lamya KaddorDIE GRÜNEN - Clankriminalität
-        "https://de.openparliament.tv/api/v1/media/DE-0200033073",
-        # Peggy SchierenbeckSPD - Kriminalität in Bahnhöfen und Zügen
-        "https://de.openparliament.tv/api/v1/media/DE-0200087058",
-    ]
 
     array_of_analysis_results = []
 
-    for speech_url in speech_urls:
-        analysis_results = analyse_speech(speech_url, sentiment_model)
+    for speech in speeches:
+        analysis_results = analyse_speech(speech, sentiment_model)
         array_of_analysis_results.append(analysis_results)
-        print("Analysis results for speech: " + speech_url)
-        print(analysis_results)
+        print(speech.get_speech_metadata())
 
-    create_csv_from_analysis_results(array_of_analysis_results)
+    create_csv_from_analysis_results(array_of_analysis_results, "budget_query_results.csv")
+
+    # sentiment_model = SentimentModel()
+    # speech_urls = [
+    #     # Martin SichertAfD - Impfpflicht gegen SARS-CoV-2 https://de.openparliament.tv/media/DE-0200028012?q=AFD&factionID%5B%5D=Q42575708
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200028012",
+    #     # Nicole HöchstAfD - Akademische und berufliche Bildung https://de.openparliament.tv/media/DE-0200103063?q=AFD&factionID%5B%5D=Q42575708
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200103063",
+    #     # Gottfried CurioAfD - Durchsetzung des Asyl- und Aufenthaltsrechts https://de.openparliament.tv/media/DE-0200103033?q=AFD&factionID%5B%5D=Q42575708
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200103033",
+    #     # NOT AFD https://de.openparliament.tv/api/v1/media/DE-0190204119
+    #     "https://de.openparliament.tv/api/v1/media/DE-0190204119",
+    #     # Martin HessAfD - Kriminalität in Bahnhöfen und Zügen https://de.openparliament.tv/media/DE-0200087057?q=Jahr+2022+um+38%2C6%25+von+rund&factionID%5B%5D=Q42575708&dateFrom=2022-10-15
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200087057",
+    #     # Lamya KaddorDIE GRÜNEN - Clankriminalität
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200033073",
+    #     # Peggy SchierenbeckSPD - Kriminalität in Bahnhöfen und Zügen
+    #     "https://de.openparliament.tv/api/v1/media/DE-0200087058",
+    # ]
+
+    # array_of_analysis_results = []
+
+    # for speech_url in speech_urls:
+    #     speech = Speech(url=speech_url)
+    #     analysis_results = analyse_speech(speech, sentiment_model)
+    #     array_of_analysis_results.append(analysis_results)
+    #     print("Analysis results for speech: " + speech_url)
+    #     print(analysis_results)
+
+    # create_csv_from_analysis_results(array_of_analysis_results)
 
     # # Now to make a plot!
 
